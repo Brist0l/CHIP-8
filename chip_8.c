08d7ac8f33e0aa0054da8e8ddce60e315fe7a655
Bris0l <gautamthegreat21@gmail.com>
2025-10-11 23:45:05 +0530

    Instruction Decoding complete

--- chip_8.c
+++ chip_8.c
@@ -40,7 +40,7 @@ void logmsg(const char* function_name,bool start,bool debug_flag);
 void _fontset();
 void _memoryframe(unsigned _BitInt(12) start,unsigned _BitInt(12) end);
 void _fillopcode();
-unsigned short fetch(_registers registers);
+const unsigned short fetch(_registers registers);
 
 void logmsg(const char* function_name,bool start,bool debug_flag){
 	if(!debug_flag)
@@ -130,10 +130,10 @@ void _fillopcode(){
 	// Filling it with a const opcode , 00EE => basically it's C's `return`
 	// So all the opcodes are 2 bytes long and it's stored in big-endian format 
 	memory[0x200] = 0x00;
-	memory[0x201] = 0xEE;
+	memory[0x201] = 0xE1;
 }
 
-unsigned short fetch(_registers registers){
+const unsigned short fetch(_registers registers){
 	logmsg("fetch",true,debug_flag);
 	_fillopcode();
 
@@ -155,11 +155,142 @@ unsigned short fetch(_registers registers){
 	return opcode;
 }
 
+/* The opcode's nibbles(4 bits) have different meaning. The first nibble tells what category of 
+ * instruction is it.
+ *
+ * The 2'nd Nibble is `X` which tells which register needs to be used (where `X` is the number. i.e.
+ * Vx)
+ * The 3'rd Nibble is `Y` which tells which is the 2nd register to be used(where `Y` is the number. i.e.
+ * Vy)
+ * Note that both `X` and `Y` can range from 0-F.
+ * The 4'th Nibble is `N` and it's a const number 
+ * 
+ * `NN` The 2nd byte (i.e. 3rd and 4th nibble) is a const number
+ * `NNN` The 2nd,3rd and 4th nibbles are an address (i.e. a 12 bit memory address)
+ */
+void execute(const unsigned short opcode){
+	logmsg("execute",true,debug_flag);
+	unsigned short first_nibble = 0xF000;
+	unsigned short second_nibble = 0x0F00;
+	unsigned short third_nibble = 0x00F0;
+	unsigned short fourth_nibble = 0x000F;
+
+	if(debug_flag){
+		printf("First nibble before: %016b(%X)\n",first_nibble,first_nibble);
+		printf("Second nibble before: %016b(%X)\n",second_nibble,second_nibble);
+		printf("Third nibble before: %016b(%X)\n",third_nibble,third_nibble);
+		printf("Fourth nibble before: %016b(%X)\n",fourth_nibble,fourth_nibble);
+	}
+
+	/* Basically this gets the first nibble because the starting 4 bits are 1s and it will turn 
+	 * itself into the first 4 bits of the opcode by ANDing it together and the rest are 0s so 
+	 * whatever be the case they will always remain 0.
+	 */
+	first_nibble &= opcode; 
+	first_nibble >>= 12;
+	second_nibble &= opcode; 
+	second_nibble >>= 8;
+	third_nibble &= opcode; 
+	third_nibble >>= 4;
+	fourth_nibble &= opcode; 
+
+	if(debug_flag){
+		printf("First nibble after: %016b(%X)\n",first_nibble,first_nibble);
+		printf("Second nibble after: %016b(%X)\n",second_nibble,second_nibble);
+		printf("Third nibble after: %016b(%X)\n",third_nibble,third_nibble);
+		printf("Fourth nibble after: %016b(%X)\n",fourth_nibble,fourth_nibble);
+	}
+
+	switch(first_nibble){
+		case 0:
+			/* Valid instructions:
+			 * 00E0 => Clears the screen
+			 * 00EE => Returns from the subroutine
+			 */
+
+			printf("Welcome to case 0 bitch\n");
+			switch(fourth_nibble){
+				case 0:
+					printf("Clearing screen\n");
+					break;
+				case 0xE:
+					printf("returing\n");
+					break;
+				default:
+					printf("Bad instruction\n");
+			}
+			
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 1:
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 2:
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 3:
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 4:
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 5:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 6:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 7:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 8:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 9:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 0xA:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 0xB:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 0xC:
+			printf("Welcome to case C bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 0xD:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 0xE:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+		case 0xF:
+			printf("Welcome to case 0 bitch\n");
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+
+		default:
+			printf("First nibble after: %016b\n",first_nibble);
+			break;
+	}
+
+	logmsg("execute",false,debug_flag);
+}
 
 int main(){
 	_registers registers;
 	registers.PC = 0x200; // starting from the unreserved section
 	_fontset();
 	_memoryframe(0x050,0x200);	
-	fetch(registers);
+	execute(fetch(registers));
 }
